13df781dd38607262ea0a5b16893872044032831
mahes13 <98155711+mahes13@users.noreply.github.com>
2023-12-26 16:50:50 +0530

    Update DataProvider.jsx

--- src/context/DataProvider.jsx
+++ src/context/DataProvider.jsx
@@ -5,9 +5,9 @@ export const DataContext = createContext(null);
 
 const DataProvider = ({ children }) => {
     
-    const [html, setHtml] = useState('');
+    const [html, setHtml] = useState('<h1 id="h1"><marquee>React Code Editor by Mahesh</marquee></h1><h2>Start Your Practice...</h2>');
     const [js, setJs] = useState('');
-    const [css, setCss] = useState('');
+    const [css, setCss] = useState('#h1{color:rgb(136, 136, 222)}');
 
     return (
         <DataContext.Provider value={{
